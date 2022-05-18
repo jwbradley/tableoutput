e01dc0e959fdda58b081d062d22d215135509e9d
James W. Bradley <jwbradley@users.noreply.github.com>
2022-05-18 15:48:45 -0500

    Comments Out
    Removed the extra comments

--- start.php
+++ start.php
@@ -23,10 +23,7 @@ $outputData   =  new getContents('https://www.alphavantage.co/query?function='.$
 $thisneedsvar =  json_decode($outputData->getFileContents(), true);
 foreach ($thisneedsvar['Time Series (5min)'] as $key => $value) {
 		echo "\n<!--- give me the key \n";var_export($key); echo " -->\n";
-	// foreach ($value as $key2 => $value2) {
 
-		// echo "\n<!--- give me the key2 \n";var_export($key2); echo " -->\n";
-		// $buildArray[$counter++]
 		$buildArray[$counter]['dattim']       = $key;
 		$buildArray[$counter]['open price']   = $value['1. open'];
 		$buildArray[$counter]['high price']   = $value['2. high'];
@@ -34,31 +31,11 @@ foreach ($thisneedsvar['Time Series (5min)'] as $key => $value) {
 		$buildArray[$counter]['close price']  = $value['4. close'];
 		$buildArray[$counter]['volume']       = $value['5. volume'];
 		$counter++;
-	// }
 }
-
-// echo "\n<!--- \$thisneedsvar['Meta Data'] is set to be\n";var_export($thisneedsvar['Meta Data']); echo " -->\n";
-// echo "\n<!--- \$thisneedsvar['Time Series (5min)'] is set to be\n";var_export($thisneedsvar['Time Series (5min)']); echo " -->\n";
-
 
 echo "\n<!--- \$buildArray is set to be\n";var_export($buildArray); echo " -->\n";
 
 
 $tabledata    =  new DtaTbleOut('stockchart', $buildArray, $header);
-
-
-// echo "\n<!--- header?\n";
-
-// var_export($tabledata->headers);
-// // var_export(json_decode($outputData->getFileContents()->"Time Series (5min)", true));
-// echo "\nheader?   -->\n";
-
-// $json = file_get_contents('https://www.alphavantage.co/query?function=KAMA&symbol=IBM&interval=weekly&time_period=10&series_type=open&apikey=demo');
-// $json = file_get_contents('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=TSLA&interval=5min&apikey=KGOFSXPNMA7WZL1V');
-
-// $data = json_decode($json,true);
-
-// print_r($data);
-
 
 ?>
